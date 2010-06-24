87b353c7ebb97a825722f598bd711bff3110861c
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-06-24 13:35:39 +0000

    more logic from eea.dataservice

--- eea/versions/versions.py
+++ eea/versions/versions.py
@@ -375,3 +375,20 @@ def generateNewId(context, id, uid):
                     id = new_id
                     break
     return id
+
+def versionIdHandler(obj, event):
+    """ Set a versionId as annotation without setting the
+        version marker interface just to have a perma link
+        to last version
+    """
+    hasVersions = obj.unrestrictedTraverse('@@hasVersions')
+    if not hasVersions():
+        verId = _get_random(10)
+        anno = IAnnotations(obj)
+        ver = anno.get(VERSION_ID)
+        #TODO: tests fails with ver = None when adding an EEAFigure,
+        #      remove "if ver:" after fix
+        if ver:
+            if not ver.values()[0]:
+                ver[VERSION_ID] = verId
+                _reindex(obj)
