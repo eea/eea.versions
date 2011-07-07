e172d4566989e1d258c93ab5ff7fa2c1c255e347
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-07-07 13:11:05 +0000

    Restored eea.versions work lost due the SVN server crash.

--- eea/versions/catalog.py
+++ eea/versions/catalog.py
@@ -1,11 +1,5 @@
 """catalog utils
 """
-# -*- coding: utf-8 -*-
-
-
-__author__ = """European Environment Agency (EEA)"""
-__docformat__ = 'plaintext'
-
 from eea.versions.interfaces import IVersionControl
 from eea.versions.interfaces import IVersionEnhanced
 from plone.indexer.decorator import indexer
@@ -17,7 +11,7 @@ def getVersionIdForIndex(obj):
     try:
         ver = IVersionControl(obj)
         return ver.getVersionId()
-    except (TypeError, ValueError): #ComponentLookupError, 
+    except (TypeError, ValueError): #ComponentLookupError,
         # The catalog expects AttributeErrors when a value can't be found
         raise AttributeError
 
