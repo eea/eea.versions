ed6033d21a7da9d1e7dfe8f2de22fbfe814ab5dd
szabozo0 <szabozo0@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-10-31 08:35:24 +0000

    make jslint happy

--- eea/versions/skins/versions_templates/createversion.js
+++ eea/versions/skins/versions_templates/createversion.js
@@ -1,4 +1,14 @@
 var newURL = "";
+function checkNewVersion(){
+    $.ajax({url     : newURL,
+            success : function(){
+                $.fancybox('<div style="text-align:center;width:250px;"><span>The new version was created, you can see it by clicking on the following link:</span><br/><br/><a href="'+newURL+'">'+newURL+'</a></div>',{'modal':false});
+            },
+            error : function(xhr, ajaxOptions, thrownError){
+                setTimeout("checkNewVersion()",5000);
+            }
+    });
+}
 function startCreationOfNewVersion(){
     $.fancybox('<div style="text-align:center;width:250px;"><span>Please wait, a new version is being created.</span><br/><br/><img src="++resource++jqzoom/zoomloader.gif"/></div>',{'modal':true});
     $.ajax({url     : context_url+"/@@createVersionId",
@@ -9,14 +19,4 @@ function startCreationOfNewVersion(){
             }
     });
 }
-        
-function checkNewVersion(){
-    $.ajax({url     : newURL,
-            success : function(){
-                $.fancybox('<div style="text-align:center;width:250px;"><span>The new version was created, you can see it by clicking on the following link:</span><br/><br/><a href="'+newURL+'">'+newURL+'</a></div>',{'modal':false});
-            },
-            error : function(xhr, ajaxOptions, thrownError){
-                setTimeout('checkNewVersion()',5000);
-            }
-    });
-}
+
