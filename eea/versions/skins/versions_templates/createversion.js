732944b26ff1106b71817ea331697c61db694f1d
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-01-30 13:05:54 +0000

    Fix small spelling error in versioning dialog

--- eea/versions/skins/versions_templates/createversion.js
+++ eea/versions/skins/versions_templates/createversion.js
@@ -8,7 +8,7 @@ function checkLatestVersion(){
         }
         else{
           jQuery.fancybox('<div style="text-align:center;width:250px;">'+
-            '<span>The new version was created, you can see'+
+            '<span>The new version was created, you can see '+
             'it by clicking on the following link:</span><br/><br/>'+
             '<a href="'+data+'">'+data+'</a></div>',
             {'modal':false}
