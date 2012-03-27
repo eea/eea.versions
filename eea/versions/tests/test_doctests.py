31172f32b39760d9701aa1ca886c52aec05dc615
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-03-27 13:11:03 +0000

    Refs #5033; added test to test for speed improvement of collective.indexing

--- eea/versions/tests/test_doctests.py
+++ eea/versions/tests/test_doctests.py
@@ -1,13 +1,77 @@
 """ Doc tests
 """
-import doctest
-import unittest
-from eea.versions.tests.base import VersionsFunctionalTestCase
+from Products.PloneTestCase.PloneTestCase import PloneTestCase
 from Testing.ZopeTestCase import FunctionalDocFileSuite as Suite
+from eea.versions.tests.base import VersionsFunctionalTestCase
+from Zope2.App.zcml import load_config
+import collective.indexing
+import doctest
+import timeit
+import unittest
+import random
 
 OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
                doctest.ELLIPSIS |
                doctest.NORMALIZE_WHITESPACE)
+
+
+class OptimizationTest(PloneTestCase):
+
+    def _make_tree(self, container):
+        make_id = lambda: "o" + str(random.randint(0,10000000000))
+
+        toplevel = container[container.invokeFactory("Folder", make_id())]
+        print "Created top level"
+
+        #add 5 folders to the top level container
+        for id in (make_id() for i in range(5)):
+            branch = toplevel[toplevel.invokeFactory("Folder", id)]
+            print "Created branch"
+
+            #add another 3 folders inside, each with 5 docs
+            for id in (make_id() for i in range(3)):
+                twig = branch[branch.invokeFactory("Folder", id)]
+                print "Created twig"
+
+                for id in (make_id() for i in range(5)):
+                    leaf = twig[twig.invokeFactory("Sample Data", id)]
+                    print "Created sample data"
+                    form = {
+                      'title': 'Dataset'+id,
+                      'description': 'Organisation description',
+                      'somedata':'Some Data',
+                    }
+                    leaf.processForm(values=form, data=1, metadata=1)
+                    print "Edited sample data"
+
+        return toplevel
+
+    def make_versions(self):
+        print "Start tree creation"
+        tree = self._make_tree(self.portal)
+        print "Finished created tree"
+        version = tree.unrestrictedTraverse("@@createVersion")()
+        print "Finished versioning"
+
+    def test_indexing(self):
+        self.loginAsPortalOwner()
+        
+        timer = timeit.Timer(self.make_versions)
+
+        #default indexing
+        print "Starting creation of objects with regular indexing"
+        v1 = timer.repeat(2, number=3)
+
+        #collective.indexing based
+        load_config('configure.zcml', collective.indexing)
+        timer = timeit.Timer(self.make_versions)
+        print "Starting creation of objects with collective indexing"
+        v2 = timer.repeat(2, number=3)
+
+        print v1
+        print v2
+
+        assert(sum(v1) > sum(v2))
 
 
 def test_suite():
@@ -18,4 +82,5 @@ def test_suite():
                   optionflags=OPTIONFLAGS,
                   package='eea.versions',
                   test_class=VersionsFunctionalTestCase) ,
+              unittest.makeSuite(OptimizationTest),
               ))
