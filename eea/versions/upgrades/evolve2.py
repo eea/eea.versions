abd54260175be404be90b5b3b6dc7a41ac26bac4
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2013-09-17 14:25:56 +0300

    Refs #16751 EEA; added evolve script

---
+++ eea/versions/upgrades/evolve2.py
@@ -0,0 +1,73 @@
+from eea.versions.interfaces import IVersionEnhanced
+from zope.annotation.interfaces import IAnnotations
+from zope.component import alsoProvides
+import logging
+import transaction
+
+logger = logging.getLogger('eea.versions.migration')
+
+
+def migrate_versionId_storage(obj):
+    """Migrate storage of versionId
+    """
+
+    old_storage = obj.__annotations__.get('versionId')
+
+    versionId = obj.__annotations__['versionId']['versionId'].strip()
+
+    #doesn't have a good versionId (could be empty string),
+    if not versionId and IVersionEnhanced.providedBy(obj):
+        obj.__annotations__['versionId'] = _random_id(obj)
+    else:
+        obj.__annotations__['versionId'] = versionId
+
+    msg = "Migrated versionId storage (old version) for %s (%s)" % \
+            (obj.absolute_url(), versionId)
+
+    logger.info(msg)
+
+
+def evolve(context):
+    cat = context.portal_catalog
+    brains = cat.searchResults(missing=True, Language="all")
+
+    for brain in brains:
+        obj = brain.getObject()
+
+        # first, check the brain's versionId
+        brain_version = brain.getVersionId
+        if isinstance(brain_version, basestring) and brain_version:
+            # everything fine
+            continue
+
+        if brain_version.portal_type == "Discussion Item":
+            continue    # skipping Discussion Items, they can't be reindexed
+
+        if isinstance(brain_version, basestring) and not brain_version.strip():
+            # an empty string, assigning new versionId
+            IAnnotations(obj)[VERSION_ID] = _random_id(obj)
+            obj.reindexObject(idxs=['getVersionId'])
+            msg = "Migrated versionId storage (empty string) for %s (%s)" % \
+                    (obj.absolute_url(), versionId)
+            logger.info(msg)
+            transaction.savepoint()
+            continue
+
+        versionId = IGetVersions(obj).versionId
+        if isinstance(versionId, basestring) and not versionId.strip():
+            # an empty string, assigning new versionId
+            IAnnotations(obj)[VERSION_ID] = _random_id(obj)
+            obj.reindexObject(idxs=['getVersionId'])
+            msg = "Migrated versionId storage (empty string) for %s (%s)" % \
+                    (obj.absolute_url(), versionId)
+            logger.info(msg)
+            transaction.savepoint()
+            continue
+
+        if not brain.getVersionId:
+            IAnnotations(obj)[VERSION_ID] = _random_id(obj)
+            obj.reindexObject(idxs=['getVersionId'])
+            transaction.savepoint()
+            continue
+
+        migrate_versionId_storage(obj)  #this is an old storage:
