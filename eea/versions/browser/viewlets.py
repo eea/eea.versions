e86fb7d5657827ce4ce01365112414fc31e1881d
David Ichim <ichim.david@gmail.com>
2017-04-28 15:54:40 +0300

    Refs #83306 added branch with initial code for canonical url change for archived objects

--- eea/versions/browser/viewlets.py
+++ eea/versions/browser/viewlets.py
@@ -1,7 +1,10 @@
 """ eea.versions viewlets
 """
 from plone.app.layout.viewlets.common import ViewletBase
+from plone.memoize import view
 from zope.component import getMultiAdapter
+from eea.versions.utils import object_provides
+from eea.versions.interfaces import IVersionControl
 
 
 class VersionStatusViewlet(ViewletBase):
@@ -15,3 +18,25 @@ class VersionStatusViewlet(ViewletBase):
         plone = getMultiAdapter((self.context, self.request),
                                 name=u'plone_context_state')
         return plone.is_view_template()
+
+
+class CanonicalURL(ViewletBase):
+    """ Override to set canonical url for archived objects
+    """
+
+    @view.memoize
+    def render(self):
+        archive = 'eea.workflow.interfaces.IObjectArchived'
+        archived = object_provides(self.context, archive)
+        canonical_url = self.context.absolute_url()
+        if archived:
+            vobj = IVersionControl(self.context)
+            if not vobj.isLatest():
+                latest = vobj.latest_version().absolute_url()
+                canonical_url = latest.absolute_url()
+            # versions = vobj.versions
+        else:
+            context_state = getMultiAdapter(
+                (self.context, self.request), name=u'plone_context_state')
+            canonical_url = context_state.canonical_object_url()
+        return u'    <link rel="canonical" href="%s" />' % canonical_url
